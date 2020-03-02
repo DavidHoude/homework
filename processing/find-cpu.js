b0cac650cf265f3d924b19c8a28d5eff5993dee1
work <work@MyLenovo.lan>
2020-03-02 18:50:02 -0500

    Added basic examples of two quick and dirty methods to extract CPU requests from a nested YML file

---
+++ processing/find-cpu.js
@@ -0,0 +1,32 @@
+var glob = require( 'glob' );  
+var yaml = require('js-yaml');
+var fs   = require('fs');
+
+// Find deployment.yaml in sub directory
+glob('deployments/**/deployment.yaml', function(err, files) {
+    for (file in files) {
+      try {
+          // Load the YAML from file, pull CPU requests from first container (no sidecars)
+          var yamlData = yaml.safeLoad(fs.readFileSync(files[file], 'utf8'));
+          let cpu = JSON.stringify(yamlData.spec.template.spec.containers[0].resources.requests.cpu).replace(/['"]+/g, '')
+
+          // Split the file path into client/product/app
+          const path = files[file].toString().split('/');
+          let client = path[1]
+          let product = path[2]
+          let app = path[3]
+
+          // Multiple whole CPU cores by 1000, or strip the 'm' from the microcores
+          if(cpu.endsWith("m")) {
+              cpu = cpu.replace('m', '')
+          } else {
+              cpu = cpu * 1000
+          }
+
+          // Output in CSV without plugin due to low complexity.
+          console.log(client + "," + product + "," + app + "," + cpu);
+      } catch (error) {
+          console.log(error);
+      }
+    }
+});
---
+++ processing/find-cpu.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Example: Traverse the deployment/ directory looking for deployment.yaml and
+# extract the total CPU requests from the file, export to CSV.
+
+for depl in $(find deployments | grep deployment.yaml); do 
+  CLIENT=$(echo $depl | awk -F '/' '{ print $2}')
+  PRODUCT=$(echo $depl | awk -F '/' '{ print $3}')
+  APP=$(echo $depl | awk -F '/' '{ print $4}')
+
+  # Use grep/awk/sed to pull requests.cpu from the YAML
+  RAWCPU=$(grep "[[:blank:]]*requests:$" $depl -A 2 | grep "[[:blank:]]*cpu: " | awk -F ':' '{print $2}' | sed 's/ //g')
+  for cpu in ${RAWCPU}; do
+
+    # 1 CPU can be defined as 1 or 1000m, this will convert the two to the same
+    if [[ ${cpu} == *m ]]; then
+      CPU=$(echo ${cpu} | sed 's/m//g')
+    else
+      CPU=$(echo "${cpu} * 1000" | bc)
+    fi
+  done
+
+  echo $CLIENT,$PRODUCT,$APP,$CPU
+done
